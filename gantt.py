70945e5dda0a3538ead135b411a468618c68a36f
Aleksei Zakharov <zaharov@selectel.ru>
2020-09-02 14:50:27 +0300

    Some basic stuff

--- gantt.py
+++ gantt.py
@@ -5,6 +5,7 @@ import requests
 from jira import JIRA
 import plotly.express as px
 import pandas as pd
+from datetime import datetime,timezone,timedelta
 
 def search(jira,label):
     start_at = 0
@@ -31,12 +32,33 @@ def work(args):
     options = {"server": "https://"+args.jira_url}
     jira = JIRA(options,auth=tuple(auth))
     issues = search(jira,args.label)
+    histories_list = list()
     for issue in issues:
-        print(issue.key + " " + issue.fields.assignee.name)
+        issue_full_hist = list()
         for history in issue.changelog.histories:
             for item in history.items:
-                if item.field == "status":
-                    print(history.created+": "+item.fromString+" => "+item.toString)
+                if item.field == "status": # "Backlog" "Analyze" "In Progress" "Testing" "Closed"
+                    issue_hist = dict()
+                    date = datetime.strptime(history.created, "%Y-%m-%dT%H:%M:%S.%f%z")
+                    issue_hist['Assignee'] = issue.fields.assignee.displayName
+                    issue_hist['Task'] = issue.key
+                    issue_hist['Status'] = item.toString
+                    issue_hist['Start'] = datetime.combine(date.date(), date.time()).isoformat(' ',timespec='minutes')
+                    for issue_hist_saved in issue_full_hist:
+                        if issue_hist_saved.get('Status') == item.fromString:
+                            issue_hist_saved['Finish'] = datetime.combine(date.date(), date.time()).isoformat(' ',timespec='minutes')
+                    issue_full_hist.append(issue_hist)
+        histories_list += issue_full_hist
+
+    for hist in histories_list:
+        if hist.get('Finish') == None:
+            hist['Finish'] = datetime.now().isoformat(' ',timespec='minutes') # TODO: Delete timezone everywhere
+    print(histories_list)
+    df = pd.DataFrame(histories_list)
+    print(df)
+    fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="Status")
+    fig.update_yaxes(autorange="reversed")
+    fig.show()
     users = parse_users(issues)
     print(users)
 
